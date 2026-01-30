293a59e512c6bae5d857769d4f02fffb353ca78c
ocviller <ocviller@student.42.fr>
2026-01-30 10:17:10 +0100

    modif main inclus 'warning'

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ocviller <ocviller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 13:12:34 by ocviller          #+#    #+#             */
-/*   Updated: 2025/12/19 11:47:32 by ocviller         ###   ########.fr       */
+/*   Updated: 2026/01/30 10:15:41 by ocviller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ int main(void)
 
     karen.complain("DEBUG");
     std::cout << "\n";
-    karen.complain("ERROR");
-    std::cout << "\n";
     karen.complain("INFO");
+    std::cout << "\n";
+    karen.complain("WARNING");
+    std::cout << "\n";
+    karen.complain("ERROR");
     std::cout << "\n";
     karen.complain("test");
     std::cout << "\n";
